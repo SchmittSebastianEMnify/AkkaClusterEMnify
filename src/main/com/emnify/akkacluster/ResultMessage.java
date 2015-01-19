7dd397e8d30f61ea924215536463244e0fc7f2af
SchmittSebastianEMnify <Sebastian.Schmitt@EMnify.com>
2015-01-19 16:56:19 +0100

    Implemented Akka Messages

--- src/main/com/emnify/akkacluster/ResultMessage.java
+++ src/main/com/emnify/akkacluster/ResultMessage.java
@@ -4,4 +4,19 @@ import java.io.Serializable;
 
 public class ResultMessage implements Serializable {
 
+  private final String answerString;
+  private final Double senderCounterValue;
+
+  public ResultMessage(String answerString, Double CounterValue) {
+    this.answerString = answerString;
+    this.senderCounterValue = CounterValue;
+  }
+
+  public String getAnswer() {
+    return this.answerString;
+  }
+
+  public Double getSenderCounterValue() {
+    return this.senderCounterValue;
+  }
 }
