7dfa6fa4d9664dbe413ed3ace075347f19570a4b
SchmittSebastianEMnify <Sebastian.Schmitt@EMnify.com>
2015-01-20 13:52:37 +0100

    Implemented Backend and fixed config error

--- src/main/com/emnify/akkacluster/backend/BackendWorker.java
+++ src/main/com/emnify/akkacluster/backend/BackendWorker.java
@@ -1,13 +1,26 @@
 package com.emnify.akkacluster.backend;
 
+import com.emnify.akkacluster.ResultMessage;
+import com.emnify.akkacluster.StringMessage;
+
 import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
 
 public class BackendWorker extends UntypedActor {
 
+  private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+  private Long MessageCounter = (long) 0;
+
   @Override
-  public void onReceive(Object arg0) throws Exception {
-    // TODO Auto-generated method stub
-
+  public void onReceive(Object message) throws Exception {
+    if (message instanceof StringMessage) {
+      StringMessage stringMessage = (StringMessage) message;
+      MessageCounter++;
+      log.info("Received Message Number " + MessageCounter + " was: " + stringMessage.getMessage()
+          + " from: " + getSender());
+      ResultMessage resultMessage = new ResultMessage("Got it!", MessageCounter);
+      getSender().tell(resultMessage, getSelf());
+    }
   }
-
 }
