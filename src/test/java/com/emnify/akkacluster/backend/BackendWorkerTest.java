b1328b2785fe73e18c780186ecb6652115631884
SchmittSebastianEMnify <Sebastian.Schmitt@EMnify.com>
2015-01-20 14:57:42 +0100

    Updated Backend + Tests

--- src/test/java/com/emnify/akkacluster/backend/BackendWorkerTest.java
+++ src/test/java/com/emnify/akkacluster/backend/BackendWorkerTest.java
@@ -8,12 +8,10 @@ import com.typesafe.config.ConfigFactory;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
-import akka.actor.Status;
 import akka.testkit.JavaTestKit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.omg.CORBA.INVALID_TRANSACTION;
 
 /**
  * @author nikos
@@ -70,22 +68,6 @@ public class BackendWorkerTest {
 
         service.tell(1, probe);
         expectNoMsg();
-      }
-    };
-  }
-
-  /**
-   * Invalid message Type -> Status.Failure response
-   */
-  @Test
-  public void FailedStatus() {
-    new JavaTestKit(system) {
-      {
-        final ActorRef service = system.actorOf(Props.create(BackendWorker.class));
-        final ActorRef probe = getRef();
-
-        service.tell(new INVALID_TRANSACTION(), probe);
-        expectMsgClass(Status.Failure.class);
       }
     };
   }
