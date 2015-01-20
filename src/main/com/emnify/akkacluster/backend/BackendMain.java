97de9bc90f13e17ff4fba8ddd32f614936ae40a9
SchmittSebastianEMnify <Sebastian.Schmitt@EMnify.com>
2015-01-20 13:52:37 +0100

    Implemented Backend and fixed config error

--- src/main/com/emnify/akkacluster/backend/BackendMain.java
+++ src/main/com/emnify/akkacluster/backend/BackendMain.java
@@ -1,10 +1,18 @@
 package com.emnify.akkacluster.backend;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
 public class BackendMain {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    Config config = ConfigFactory.load("backend");
+    ActorSystem system = ActorSystem.create("clusterSystem", config);
 
+    system.actorOf(Props.create(BackendSupervisor.class), "backendSupervisor");
   }
 
 }
--- src/main/com/emnify/akkacluster/backend/BackendSupervisor.java
+++ src/main/com/emnify/akkacluster/backend/BackendSupervisor.java
@@ -1,12 +1,52 @@
 package com.emnify.akkacluster.backend;
 
+import com.emnify.akkacluster.StringMessage;
+
+import akka.actor.ActorRef;
+import akka.actor.OneForOneStrategy;
+import akka.actor.Props;
+import akka.actor.SupervisorStrategy;
+import akka.actor.SupervisorStrategy.Directive;
 import akka.actor.UntypedActor;
+import akka.japi.Function;
+import akka.routing.FromConfig;
+import scala.concurrent.duration.Duration;
 
 public class BackendSupervisor extends UntypedActor {
 
+  ActorRef backendRouter = getContext().actorOf(
+      FromConfig.getInstance().props(Props.create(BackendWorker.class)), "backendRouter");
+
+  private static SupervisorStrategy strategy = new OneForOneStrategy(10,
+      Duration.create("1 minute"), new Function<Throwable, Directive>() {
+        @Override
+        public Directive apply(Throwable t) {
+          if (t instanceof ArithmeticException) {
+            return akka.actor.SupervisorStrategy.resume();
+          } else if (t instanceof NullPointerException) {
+            return akka.actor.SupervisorStrategy.restart();
+          } else if (t instanceof IllegalArgumentException) {
+            return akka.actor.SupervisorStrategy.stop();
+          } else {
+            return akka.actor.SupervisorStrategy.escalate();
+          }
+        }
+      });
+
   @Override
-  public void onReceive(Object arg0) throws Exception {
-    // TODO Auto-generated method stub
+  public SupervisorStrategy supervisorStrategy() {
+    return strategy;
+  }
+
+
+
+  @Override
+  public void onReceive(Object message) throws Exception {
+    if (message instanceof StringMessage) {
+      backendRouter.forward(message, getContext());
+    } else {
+      unhandled(message);
+    }
 
   }
 
