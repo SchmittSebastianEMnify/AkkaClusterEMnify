538169f32350e3c254153b7358c42e61e2d699f8
nloutas <nloutas@gmail.com>
2015-01-21 14:12:52 +0100

    moved the backend code to the correct folder

--- src/main/java/com/emnify/akkacluster/backend/BackendSupervisor.java
+++ src/main/java/com/emnify/akkacluster/backend/BackendSupervisor.java
@@ -1,12 +1,27 @@
 package com.emnify.akkacluster.backend;
 
+import com.emnify.akkacluster.StringMessage;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
+import akka.routing.FromConfig;
 
 public class BackendSupervisor extends UntypedActor {
 
+  ActorRef backendRouter = getContext().actorOf(
+      FromConfig.getInstance().props(Props.create(BackendWorker.class)), "backendRouter");
+
+  /**
+   * Forwarding the Messages through a Router
+   */
   @Override
-  public void onReceive(Object arg0) throws Exception {
-    // TODO Auto-generated method stub
+  public void onReceive(Object message) throws Exception {
+    if (message instanceof StringMessage) {
+      backendRouter.forward(message, getContext());
+    } else {
+      unhandled(message);
+    }
 
   }
 
